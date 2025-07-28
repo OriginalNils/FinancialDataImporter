af5f327da92d22a5d601674075c06a2b99a96cd1
Nils X1 <nils-doering@gmx.de>
2025-07-28 17:04:34 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="financialdataimporter",
-    version="0.1.0",
+    version="0.1.1",
     author="Nils Döring",
     author_email="ndoering@students.uni-mainz.de",
     description="Ein einfacher Importer für Finanzmarktdaten von Yahoo Finance mit Caching-Funktion.",
