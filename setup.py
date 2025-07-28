0c6f34e31c2a5ebb21f33755f364aa0d876c5ef1
Nils X1 <nils-doering@gmx.de>
2025-07-28 15:52:44 +0200

    Setup Package Changes

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="financialdataimporter",
+    version="0.1.0",
+    author="Nils Döring",
+    author_email="ndoering@students.uni-mainz.de",
+    description="Ein einfacher Importer für Finanzmarktdaten von Yahoo Finance mit Caching-Funktion.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/OriginalNils/FinancialDataImporter", # Optional: Link zum GitHub-Repo
+    
+    install_requires=[
+        "pandas>=1.0.0",  # Wir geben an, dass wir mindestens Version 1.0.0 von pandas benötigen
+        "yfinance>=0.1.63"
+    ],
+    
+    packages=find_packages(),
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Office/Business :: Financial :: Investment",
+    ],
+    python_requires='>=3.6',
+)
